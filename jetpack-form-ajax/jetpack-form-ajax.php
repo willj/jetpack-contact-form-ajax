fcbc60f990852a74c9d3b99c63a3ecd2b97abf80
Will <willj@users.noreply.github.com>
2017-08-24 11:52:25 +0100

    Add loading indicator

--- jetpack-form-ajax/jetpack-form-ajax.php
+++ jetpack-form-ajax/jetpack-form-ajax.php
@@ -7,10 +7,11 @@ Author: @willj
 
 function jfa_enqueue_scripts(){
     wp_enqueue_script( 'jfa_scripts', plugins_url( 'jetpackFormAjax.js', __FILE__ ), array('jquery') );    
+    wp_enqueue_style( 'jfa_styles', plugins_url( 'jetpackFormAjax.css', __FILE__ ) );    
 }
 add_action( 'wp_enqueue_scripts', 'jfa_enqueue_scripts' );
 
-function jfa_success_message() {
+function jfa_success_message(){
     return '<p class="jetpack-form-ajax-success-message">Message sent</p>';
 }
 add_filter( 'grunion_contact_form_success_message', 'jfa_success_message' );
--- jetpack-form-ajax/jetpackFormAjax.js
+++ jetpack-form-ajax/jetpackFormAjax.js
@@ -7,23 +7,40 @@
         var formId = form.find('[name="contact-form-id"]').val();
         var statusWrapper = form.find('.jetpack-form-ajax-status-wrapper');
 
-        statusWrapper.empty();
+        form.addClass('jetpack-form-ajax-is-loading');
 
         $.post(form.attr('action'), form.serialize(), function(response){
             var errors = $(response).find('ul.form-errors');
 
+            statusWrapper.empty();
             if (errors.length > 0){
                 statusWrapper.append(errors);
             } else {
                 $(response).find('.jetpack-form-ajax-success-message').appendTo(statusWrapper);
             }
+        }).always(function(){
+            form.removeClass('jetpack-form-ajax-is-loading');
         });
+    }
+
+    function replaceSubmitButtons(){
+        var btns = $('.contact-form > .contact-submit > input[type="submit"]');
+        
+        for(var i = 0; i < btns.length; i++){
+            var loadingIndicator = $('<span class="jetpack-form-ajax-loading"><span>●</span><span>●</span><span>●</span></span>');
+            var oldBtn = $(btns[i]);
+            var newBtn = $('<button />').attr({ type: 'submit', class: oldBtn.attr('class') }).addClass('jetpack-form-ajax-button').html(oldBtn.val()).append(loadingIndicator);
+            
+            oldBtn.replaceWith(newBtn);
+        }
     }
 
     $(document).ready(function(){
         $('<div />').attr({ class: 'jetpack-form-ajax-status-wrapper' }).insertBefore('.contact-submit');
         
+        replaceSubmitButtons();
+
         $('.contact-form').on('submit', handleFormSubmit);
     });
-    
+
 })(jQuery);
